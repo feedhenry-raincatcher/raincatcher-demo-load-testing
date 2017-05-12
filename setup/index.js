46936e1268138aa146eb9b643547febe1c5828a7
witmicko <mogrodni@redhat.com>
2017-05-12 09:35:27 +0100

    addded login to setup

--- setup/index.js
+++ setup/index.js
@@ -3,11 +3,8 @@
 const usersWorkorders = require('./usersWorkorders');
 const dataReset = require('./dataReset');
 const login  = require('./login');
-const yargopts = require('../util/yargopts');
 
 
-login().then(token => {
-  dataReset(token, yargopts.argv);
-}).then(usersWorkorders);
+login().then(dataReset).then(usersWorkorders);
 
 
